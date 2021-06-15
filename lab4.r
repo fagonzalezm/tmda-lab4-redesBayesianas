4a288920b832ce2ffda120d1afe2b815bbce0b49
Felipe González M <felipe.gonzalez.me@usach.cl>
2021-06-15 15:45:13 -0400

    ADD/ mmhc and hc

--- lab4.r
+++ lab4.r
@@ -8,9 +8,13 @@ summary(bn_df)
 # Datos categóricos
 # Los datos son factores de hasta 5 niveles
 
-res <- hc(bn_df) #Algoritmo Hill-Climbing
+#
+bn_df <- bn_df[,-13] # Se elimina DrivSkill porque es muy parecido a drivQuality, es redundante
+bn_df <- bn_df[,-15] # Se elimina Theft porque son casi puros false
 
-viewer(res,
+hc <- hc(bn_df) #Algoritmo Hill-Climbing
+
+viewer(hc,
        bayesianNetwork.width = "100%",
        bayesianNetwork.height = "80vh",
        bayesianNetwork.layout = "layout_on_grid",
@@ -24,13 +28,54 @@ viewer(res,
                                            border = "#008f39"))
 )
 
+hc.sc<-score(hc,bn_df) # BIC por default
+print(hc.sc)
+
+# mmhc
+mmhc<-mmhc(bn_df)
+mmhc.sc <- score(mmhc,bn_df)
+print(mmhc.sc)
+viewer(mmhc,
+       bayesianNetwork.width = "100%",
+       bayesianNetwork.height = "80vh",
+       bayesianNetwork.layout = "layout_on_grid",
+       bayesianNetwork.title="Discrete Bayesian Network - Insurance MMHC",
+       bayesianNetwork.subtitle = "Car insurance",
+       bayesianNetwork.footer = "Fig. 2 - Layout on grid",
+       
+       node.colors = list(background = "#00722e",
+                          border = "#484848",
+                          highlight = list(background = "#00b347",
+                                           border = "#008f39"))
+)
+
+# mmpc
+mmpc<-mmpc(bn_df)
+#mmpc.sc <- score(mmpc,bn_df)
+viewer(mmpc,
+       bayesianNetwork.width = "100%",
+       bayesianNetwork.height = "80vh",
+       bayesianNetwork.layout = "layout_on_grid",
+       bayesianNetwork.title="Discrete Bayesian Network - Insurance MMPC",
+       bayesianNetwork.subtitle = "Car insurance",
+       bayesianNetwork.footer = "Fig. 2 - Layout on grid",
+       
+       node.colors = list(background = "#00722e",
+                          border = "#484848",
+                          highlight = list(background = "#00b347",
+                                           border = "#008f39"))
+)
+
+# hc tiene mator BIC, se elige ese método
+# Se creó una blacklist a partir del modelo hc y conocimiento sobre el problema.
 bl <- matrix(c("ThisCarCost","ThisCarDam","ThisCarDam","ThisCarCost","MedCost","ThisCarDam","MakeModel","VehicleYear","VehicleYear","MakeModel","VehicleYear","RiskAversion","RiskAversion","VehicleYear","SocioEcon","OtherCar","OtherCar","SocioEcon","RuggedAuto","MakeModel","MakeModel","OtherCar","OtherCar","MakeModel","Mileage","ThisCarDam","ThisCarDam","Mileage","RiskAversion","HomeBase","Airbag","Cushioning","Cushioning","Airbag"),ncol=2,byrow=TRUE)
+print(bl)
 res <- hc(bn_df,blacklist = bl)
 viewer(res,
        bayesianNetwork.width = "100%",
        bayesianNetwork.height = "80vh",
        bayesianNetwork.layout = "layout_on_grid",
-       bayesianNetwork.title="Discrete Bayesian Network - Insurance",
+       bayesianNetwork.title="Discrete Bayesian Network - Insurance HC",
        bayesianNetwork.subtitle = "Car insurance",
        bayesianNetwork.footer = "Fig. 2 - Layout on grid",
        
@@ -39,55 +84,9 @@ viewer(res,
                           highlight = list(background = "#00b347",
                                            border = "#008f39"))
 )
-
 print(res)
 
-sc<-score(res,bn_df) # BIC por default
-print(sc)
 
+###### CONSULTAS ########
 
-fittedbn <- bn.fit(res, data = bn_df) # Se obtiene la tabla de probabilidades condicionales mediante EM. (Máxima Expectación, propagación de la evidencia)
-print(fittedbn$Age) #se obtiene la información respecto del nodo Proteins
-
-
-cpquery(fittedbn, event = (Proteins=="<3"), evidence = ( Smoking=="no") ) 
-
-cpquery(fittedbn, event = (Pressure==">140"), evidence = ( Proteins=="<3" ) )
-
-require(bnlearn)
-bn_df <- data.frame(asia)
-
-
-res <- hc(bn_df)
-plot(res)
-sc<-score(res,bn_df)
-print(sc)
-
-
-fittedbn <- bn.fit(res, data = bn_df)
-print(fittedbn$E)
-
-
-cpquery(fittedbn, event = (B=="yes"), evidence = ( S=="no") ) 
-
-cpquery(fittedbn, event = (B=="yes"), evidence = ( S=="yes") )
-
-bn_df <- data.frame(asia)
-res <- mmhc(bn_df)
-plot(res)
-sc<-score(res,bn_df)
-print(sc)
-
-
-fittedbn <- bn.fit(res, data = bn_df) #idéntico al anterior
-print(fittedbn$E)
-
-cpquery(fittedbn, event = (B=="yes"), evidence = ( S=="no") )
-
-cpquery(fittedbn, event = (B=="yes"), evidence = ( S=="yes") )
-
-bn_df <- data.frame(asia)
-res <- mmpc(bn_df)
-plot(res)
-
-fittedbn <- bn.fit(res, data = bn_df) #no hay direccionalidad en los arcos
+# Problema: ¿Contar con medidas de seguridad en los autos aumenta o disminuye los accidentes automovilístiocos?¿Influye en la gravedad de estos?
